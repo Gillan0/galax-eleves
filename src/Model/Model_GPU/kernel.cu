d834c52cbf1e933c3a1cb08fcb1dafdb8c9f1e10
Gillan0 <antonino.gillard@orange.fr>
2026-03-20 23:16:11 +0100

    updated naive

--- src/Model/Model_GPU/kernel.cu
+++ src/Model/Model_GPU/kernel.cu
@@ -7,27 +7,40 @@
 
 __global__ void compute_acc(float3 * positionsGPU, float3 * velocitiesGPU, float3 * accelerationsGPU, float* massesGPU, int n_particles)
 {
-	unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i >= n_particles) return;
 
-	for (int j = 0; j < n_particles; j++)
-	{
-		const float diffx = positionsGPU[j].x - positionsGPU[i].x;
-		const float diffy = positionsGPU[j].y - positionsGPU[i].y;
-		const float diffz = positionsGPU[j].z - positionsGPU[i].z;
+    float ax = 0.0f;
+    float ay = 0.0f;
+    float az = 0.0f;
 
-		float dij = diffx * diffx + diffy * diffy + diffz * diffz ;
+	float posX = positionsGPU[i].x;
+	float posY = positionsGPU[i].y;
+	float posZ = positionsGPU[i].z;
 
-		dij = dij * dij * dij;
-		dij = 1.0f/sqrtf(dij);
-		dij = fminf(10.0f, 10.0f * dij);
+    for (int j = 0; j < n_particles; j++)
+    {
+        if (i == j) continue;
 
-		accelerationsGPU[i].x += diffx * dij * massesGPU[j];
-		accelerationsGPU[i].y += diffy * dij * massesGPU[j];
-		accelerationsGPU[i].z += diffz * dij * massesGPU[j];
-	
-	}
+		const float diffx = positionsGPU[j].x - posX;
+		const float diffy = positionsGPU[j].y - posY;
+		const float diffz = positionsGPU[j].z - posZ;
 
+		float dij = diffx * diffx + diffy * diffy + diffz * diffz;
+
+		dij = rsqrtf(dij);
+		dij = fminf(10.0f, 10.0f * dij * dij * dij);
+
+        ax += diffx * dij * massesGPU[j];
+        ay += diffy * dij * massesGPU[j];
+        az += diffz * dij * massesGPU[j];
+    }
+
+    accelerationsGPU[i].x += ax;
+    accelerationsGPU[i].y += ay;
+    accelerationsGPU[i].z += az;
 }
+
 
 __global__ void maj_pos(float3 * positionsGPU, float3 * velocitiesGPU, float3 * accelerationsGPU, int n_particles)
 {
@@ -36,9 +49,9 @@ __global__ void maj_pos(float3 * positionsGPU, float3 * velocitiesGPU, float3 * 
 	velocitiesGPU[i].x += accelerationsGPU[i].x * 2.0f;
 	velocitiesGPU[i].y += accelerationsGPU[i].y * 2.0f;
 	velocitiesGPU[i].z += accelerationsGPU[i].z * 2.0f;
-	positionsGPU[i].x += velocitiesGPU[i].x * 0.1f;
-	positionsGPU[i].y += velocitiesGPU[i].y * 0.1f;
-	positionsGPU[i].z += velocitiesGPU[i].z * 0.1f;
+	positionsGPU[i].x += velocitiesGPU[i].x * DIFF_T;
+	positionsGPU[i].y += velocitiesGPU[i].y * DIFF_T;
+	positionsGPU[i].z += velocitiesGPU[i].z * DIFF_T;
 
 }
 
