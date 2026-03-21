69b3c01d33d7cdc0a15f36772b580d455c9f9932
Gillan0 <antonino.gillard@orange.fr>
2026-03-21 22:06:30 +0100

    fmaf use

--- src/Model/Model_GPU/kernel.cu
+++ src/Model/Model_GPU/kernel.cu
@@ -18,22 +18,25 @@ __global__ void compute_acc(float3 * positionsGPU, float3 * velocitiesGPU, float
 	float posY = positionsGPU[i].y;
 	float posZ = positionsGPU[i].z;
 
-    for (int j = 0; j < n_particles; j++)
+    for (int j = i; j < n_particles; j++)
     {
-        if (i == j) continue;
 
 		const float diffx = positionsGPU[j].x - posX;
 		const float diffy = positionsGPU[j].y - posY;
 		const float diffz = positionsGPU[j].z - posZ;
 
-		float dij = diffx * diffx + diffy * diffy + diffz * diffz;
+		float dij = fmaf(diffx, diffx, 0.0f);
+		dij = fmaf(diffy, diffy, dij);
+		dij = fmaf(diffz, diffz, dij);
 
 		dij = rsqrtf(dij);
 		dij = fminf(10.0f, 10.0f * dij * dij * dij);
+		dij = dij *  massesGPU[j];
 
-        ax += diffx * dij * massesGPU[j];
-        ay += diffy * dij * massesGPU[j];
-        az += diffz * dij * massesGPU[j];
+        ax = fmaf(diffx, dij, ax);
+        ay = fmaf(diffz, dij, ay);
+        az = fmaf(diffy, dij, az);
+
     }
 
     accelerationsGPU[i].x += ax;
@@ -46,18 +49,18 @@ __global__ void maj_pos(float3 * positionsGPU, float3 * velocitiesGPU, float3 * 
 {
 	unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
 
-	velocitiesGPU[i].x += accelerationsGPU[i].x * 2.0f;
-	velocitiesGPU[i].y += accelerationsGPU[i].y * 2.0f;
-	velocitiesGPU[i].z += accelerationsGPU[i].z * 2.0f;
-	positionsGPU[i].x += velocitiesGPU[i].x * DIFF_T;
-	positionsGPU[i].y += velocitiesGPU[i].y * DIFF_T;
-	positionsGPU[i].z += velocitiesGPU[i].z * DIFF_T;
+	velocitiesGPU[i].x =  fmaf(accelerationsGPU[i].x, 2.0f, velocitiesGPU[i].x);
+	velocitiesGPU[i].y =  fmaf(accelerationsGPU[i].y, 2.0f, velocitiesGPU[i].y);
+	velocitiesGPU[i].z =  fmaf(accelerationsGPU[i].z, 2.0f, velocitiesGPU[i].z);
+	positionsGPU[i].x = fmaf(velocitiesGPU[i].x, DIFF_T, positionsGPU[i].x);
+	positionsGPU[i].y = fmaf(velocitiesGPU[i].y, DIFF_T, positionsGPU[i].y);
+	positionsGPU[i].z = fmaf(velocitiesGPU[i].z, DIFF_T, positionsGPU[i].z);
 
 }
 
 void update_position_cu(float3* positionsGPU, float3* velocitiesGPU, float3* accelerationsGPU, float* massesGPU, int n_particles)
 {
-	int nthreads = 128;
+	int nthreads = 256;
 	int nblocks =  (n_particles + (nthreads -1)) / nthreads;
 
 	compute_acc<<<nblocks, nthreads>>>(positionsGPU, velocitiesGPU, accelerationsGPU, massesGPU, n_particles);
